3a46c67695815902b9bdd6e6b7445d8970a05b92
aminehamrouni24 <aminhamrouni82@gmail.com>
2021-06-11 23:40:38 +0200

    first express app

---
+++ server.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const app=express();
+app.use(express.json());
+
+
+//server is only operating in working days
+app.use(addActiveTime =(req, res, next)=> {
+  
+    let hours= new Date().getHours();
+    let day= new Date().getDay();
+    if((hours>9)||(hours<17)||(day==0)||(day==6))
+  {
+    res.sendFile(__dirname+'/views/closed.html')
+    
+  }
+  else{
+      
+  next() 
+}
+}) 
+
+
+app.use(express.static('views'))
+
+const port=process.env.PORT || 5000
+app.listen(port,(err)=>{
+    if (err) console.log('server is not running') 
+    else
+    console.log(`server is running on port ${port}...`)
+});
